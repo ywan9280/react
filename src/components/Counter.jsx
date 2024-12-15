37f5cbc7927e85a90d30b24b1c46dce40d0c35a8
Yuhao <ywan9280@uni.sydney.edu.au>
2024-12-15 19:32:06 +0800

    first

---
+++ src/components/Counter.jsx
@@ -0,0 +1,11 @@
+import {useState} from "react"
+import '../style.css'
+
+const Counter = () => {
+   const [count,setCount] = useState(0);
+  return (
+    <div>Counter</div>
+  )
+}
+
+export default Counter
